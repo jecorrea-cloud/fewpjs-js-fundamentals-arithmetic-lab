d2ed9deef8bc10495fe7f62f3271cef461a61048
joaquin <jecorrea@protonmail.com>
2021-05-01 02:46:28 -0400

    commit tree ad8ac6ae93f122ec289c6d5a24922e5d7cb7dda5

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 31, num2=2, num3=24, num4=5;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*10)+1;
+
+const mod = num3%num4;
+
+const max = Math.max(19,4,1/2,9,12,20,10)
